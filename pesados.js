78d891be6e7066e0aad88111b193bc7af611eb25
Diogo Carreira <dcweb19@gmail.com>
2023-11-17 13:21:17 +0000

    update1

---
+++ pesados.js
@@ -0,0 +1,12 @@
+let vis = document.getElementById(`vis`)
+let esc = document.getElementById(`esc`)
+let vuci07 = document.getElementById(`vuci07`)
+
+function visual() {
+    if 
+    vuci07.style.visibility = 'visible'   
+}
+
+function esconde() {
+    vuci07.style.visibility = `hidden`
+}
