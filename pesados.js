fef04eb310e9c7e74c3f9f660b7f6b2d1413eff2
Diogo Carreira <dcweb19@gmail.com>
2023-11-22 16:47:06 +0000

    botao esconde imagens

--- pesados.js
+++ pesados.js
@@ -9,5 +9,15 @@ function visual() {
         vuci07.style.display = `none`
         visivel = false
     }
-    
+}
+
+function esconder() {
+    let esconde = false
+    if (esconde == false){
+        vuci07.style.display = `none`
+        esconde = true
+    } else {
+        vuci07.style.display = `block`
+        esconde = false
+    }
 }
